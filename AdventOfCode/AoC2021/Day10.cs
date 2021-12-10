01502707e9609616e2ebd732f98accd37fa96759
Saiberion <saiberion@web.de>
2021-12-10 12:05:49 +0100

    completely solved day 10 of AoC2021

--- AdventOfCode/AoC2021/Day10.cs
+++ AdventOfCode/AoC2021/Day10.cs
@@ -9,8 +9,85 @@ namespace AoC2021
     {
         override public void Solve()
         {
-            Part1Solution = "TBD";
-            Part2Solution = "TBD";
+            int errorScore = 0;
+            List<long> completeScores = new List<long>();
+            Dictionary<char, int> errorPoints = new Dictionary<char, int>();
+            errorPoints.Add(')', 3);
+            errorPoints.Add(']', 57);
+            errorPoints.Add('}', 1197);
+            errorPoints.Add('>', 25137);
+            Dictionary<char, int> completePoints = new Dictionary<char, int>();
+            completePoints.Add('(', 1);
+            completePoints.Add('[', 2);
+            completePoints.Add('{', 3);
+            completePoints.Add('<', 4);
+
+            foreach (string s in Input)
+            {
+                Stack<char> openingBrackets = new Stack<char>();
+                openingBrackets.Push(s[0]);
+                bool aborted = false;
+                for (int i = 1; i < s.Length; i++)
+                {
+                    if ((s[i] == '(') || (s[i] == '[') || (s[i] == '{') || (s[i] == '<'))
+                    {
+                        openingBrackets.Push(s[i]);
+                    }
+                    else
+                    {
+                        char lastOpen = openingBrackets.Pop();
+                        if (lastOpen == '(')
+                        {
+                            if (s[i] != ')')
+                            {
+                                errorScore += errorPoints[s[i]];
+                                aborted = true;
+                                break;
+                            }
+                        }
+                        else if (lastOpen == '[')
+                        {
+                            if (s[i] != ']')
+                            {
+                                errorScore += errorPoints[s[i]];
+                                aborted = true;
+                                break;
+                            }
+                        }
+                        else if (lastOpen == '{')
+                        {
+                            if (s[i] != '}')
+                            {
+                                errorScore += errorPoints[s[i]];
+                                aborted = true;
+                                break;
+                            }
+                        }
+                        else if (lastOpen == '<')
+                        {
+                            if (s[i] != '>')
+                            {
+                                errorScore += errorPoints[s[i]];
+                                aborted = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (aborted == false)
+                {
+                    long completeScore = 0;
+                    while (openingBrackets.Count > 0)
+                    {
+                        char bracket = openingBrackets.Pop();
+                        completeScore = completeScore * 5 + completePoints[bracket];
+                    }
+                    completeScores.Add(completeScore);
+                }
+            }
+            completeScores.Sort();
+            Part1Solution = errorScore.ToString();
+            Part2Solution = completeScores[completeScores.Count / 2].ToString();
         }
     }
 }
--- AdventOfCode/SolverUI.cs
+++ AdventOfCode/SolverUI.cs
@@ -90,7 +90,7 @@ namespace AdventOfCode
                     new AoC2021.Day07() { Enabled = true },
                     new AoC2021.Day08() { Enabled = true },
                     new AoC2021.Day09() { Enabled = true },
-                    new AoC2021.Day10() { Enabled = false },
+                    new AoC2021.Day10() { Enabled = true },
                     new AoC2021.Day11() { Enabled = false },
                     new AoC2021.Day12() { Enabled = false },
                     new AoC2021.Day13() { Enabled = false },
