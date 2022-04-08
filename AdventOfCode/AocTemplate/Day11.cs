1bf833e5eb7d650f4e435b0f74e57d38233e9760
Saiberion <saiberion@web.de>
2022-04-08 17:23:25 +0200

    Created template for future events

---
+++ AdventOfCode/AocTemplate/Day11.cs
@@ -0,0 +1,17 @@
+﻿using AdventOfCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AoCTemplate
+{
+    public class Day11 : Day
+    {
+        public override void Solve()
+        {
+            Part1Solution = "TBD";
+
+            Part2Solution = "TBD";
+        }
+    }
+}
