1a9b1acab4ebcb038cdc0fc7592a0c608d86bca6
Saiberion <saiberion@web.de>
2022-05-05 17:54:23 +0200

    Ported Day 18 of AoC 2016

---
+++ AdventOfCode/AoC2016/Day18.cs
@@ -0,0 +1,72 @@
+﻿using AdventOfCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AoC2016
+{
+    public class Day18 : Day
+    {
+        int TrapRoom(List<string> input, int rows)
+        {
+            int safeTiles = 0;
+
+            foreach (char c in input[input.Count - 1])
+            {
+                if (c == '.')
+                {
+                    safeTiles++;
+                }
+            }
+            while (input.Count < rows)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < input[input.Count - 1].Length; i++)
+                {
+                    bool left, center, right;
+                    if (i == 0)
+                    {
+                        left = false;
+                    }
+                    else
+                    {
+                        left = input[input.Count - 1][i - 1] == '^';
+                    }
+                    center = input[input.Count - 1][i] == '^';
+                    if (i == (input[input.Count - 1].Length - 1))
+                    {
+                        right = false;
+                    }
+                    else
+                    {
+                        right = input[input.Count - 1][i + 1] == '^';
+                    }
+
+                    if ((!left && !center && right)
+                        || (!left && center && right)
+                        || (left && !center && !right)
+                        || (left && center && !right))
+                    {
+                        sb.Append("^");
+                    }
+                    else
+                    {
+                        sb.Append(".");
+                        safeTiles++;
+                    }
+                }
+                input.Add(sb.ToString());
+            }
+
+            return safeTiles;
+        }
+
+        public override void Solve()
+        {
+            Part1Solution = TrapRoom(new List<string>(Input), 40).ToString();
+
+            Part2Solution = TrapRoom(new List<string>(Input), 400000).ToString();
+        }
+    }
+}
