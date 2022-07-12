7286471827ee7f329a1821d19f809e8c8ffa2099
Matthias Wieloch <saiberion@web.de>
2022-07-12 18:50:45 +0200

    prepared part 2 but brute forcing would take beyond the lifetime of the universe (over 2.8e20 years)

--- AdventOfCode/AoC2019/Day22.cs
+++ AdventOfCode/AoC2019/Day22.cs
@@ -7,59 +7,76 @@ namespace AoC2019
 {
     public class Day22 : Day
     {
+        private long ShuffleAndFollowCard(long card, long deckSize, long iterations)
+        {
+            long posOfCard = card;
+
+            for (long l = 0; l < iterations; l++)
+            {
+                foreach (string s in Input)
+                {
+                    string[] splitted = s.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (splitted[0].Equals("deal"))
+                    {
+                        if (splitted[1].Equals("into"))
+                        {
+                            posOfCard = deckSize - posOfCard - 1;
+                        }
+                        else if (splitted[1].Equals("with"))
+                        {
+                            long incr = long.Parse(splitted[3]);
+                            posOfCard = (posOfCard * incr) % deckSize;
+                        }
+                    }
+                    else if (splitted[0].Equals("cut"))
+                    {
+                        long cutAmount = long.Parse(splitted[1]);
+                        if (cutAmount > 0)
+                        {
+                            if (posOfCard < cutAmount)
+                            {
+                                posOfCard = deckSize - cutAmount + posOfCard;
+                            }
+                            else
+                            {
+                                posOfCard = posOfCard - cutAmount;
+                            }
+                        }
+                        else
+                        {
+                            if (posOfCard < (deckSize + cutAmount))
+                            {
+                                posOfCard = posOfCard - cutAmount;
+                            }
+                            else
+                            {
+                                posOfCard = posOfCard - deckSize - cutAmount;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return posOfCard;
+        }
+
+        private long ShuffleAndFollowPosition(long card, long deckSize)
+        {
+            for (long l = 0; l < deckSize; l++)
+            {
+                if (ShuffleAndFollowCard(l, deckSize, 101741582076661) == card)
+                {
+                    return l;
+                }
+            }
+            return -1;
+        }
+
         override public void Solve()
         {
-            List<int> cards = new List<int>();
-
-            for (int i = 0; i < 10007; i++)
-            {
-                cards.Add(i);
-            }
-
-            foreach(string s in Input)
-            {
-                string[] splitted = s.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-
-                if (splitted[0].Equals("deal"))
-                {
-                    if (splitted[1].Equals("into"))
-                    {
-                        cards.Reverse();
-                    }
-                    else if (splitted[1].Equals("with"))
-                    {
-                        int incr = int.Parse(splitted[3]);
-                        int[] arr = new int[cards.Count];
-                        for(int i = 0, pos = 0; i < cards.Count; i++)
-                        {
-                            arr[pos] = cards[i];
-                            pos = (pos + incr) % arr.Length;
-                        }
-                        cards.Clear();
-                        cards.AddRange(arr);
-                    }
-                }
-                else if (splitted[0].Equals("cut"))
-                {
-                    int cutAmount = int.Parse(splitted[1]);
-                    if (cutAmount > 0)
-                    {
-                        cards.AddRange(cards.GetRange(0, cutAmount));
-                        cards.RemoveRange(0, cutAmount);
-                    }
-                    else
-                    {
-                        // expect negative, as cut 0 would do nothing
-                        cutAmount = Math.Abs(cutAmount);
-                        cards.InsertRange(0, cards.GetRange(cards.Count - cutAmount, cutAmount));
-                        cards.RemoveRange(cards.Count - cutAmount, cutAmount);
-                    }
-                }
-                
-            }
-
-            Part1Solution = cards.IndexOf(2019).ToString();
-            Part2Solution = "TBD";
+            Part1Solution = ShuffleAndFollowCard(2019, 10007, 1).ToString();
+            //Part2Solution = ShuffleAndFollowPosition(2020, 119315717514047L).ToString();
         }
     }
 }
