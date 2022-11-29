2b59bacf3b9816e7adf55989934441dc1483270d
Matthias Wieloch <saiberion@web.de>
2022-11-29 15:12:15 +0100

    Added todo for solvaed part 1

--- AdventOfCode/AoC2019/Day23.cs
+++ AdventOfCode/AoC2019/Day23.cs
@@ -9,7 +9,7 @@ namespace AoC2019
     {
         override public void Solve()
         {
-            // I did solve part 1 of this but it seems I lost the code
+            // TODO I did solve part 1 of this but it seems I lost the code
             Part1Solution = "TBD";
             Part2Solution = "TBD";
         }
