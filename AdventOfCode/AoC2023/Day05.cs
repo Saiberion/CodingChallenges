e3f14331aedc79e41c1fa12e5c0756e62354c692
Saiberion <saiberion@web.de>
2023-12-05 18:31:24 +0100

    prepared for day 5 puzzle

--- AdventOfCode/AoC2023/Day05.cs
+++ AdventOfCode/AoC2023/Day05.cs
@@ -5,10 +5,53 @@ using System.Text;
 
 namespace AoC2023
 {
+    
+
     public class Day05 : Day
     {
+        private enum EInputReadType
+        {
+            none,
+            seed,
+            seedToSoilMap,
+            soilToFertilizterMap,
+            fertilizerToWater,
+            waterToLight,
+            lightToTemperature,
+            temperatureToHumidity,
+            humidityToLocation
+        }
+
         public override void Solve()
         {
+            List<int> seeds = new();
+            EInputReadType readType = EInputReadType.none;
+
+            foreach (string line in Input)
+            {
+                if (line.StartsWith("seeds:"))
+                {
+                    string[] seedsStr = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach(string seed in seedsStr)
+                    {
+                        if (char.IsDigit(seed[0]))
+                        {
+                            seeds.Add(int.Parse(seed));
+                        }
+                    }
+                }
+                else if (line.StartsWith("seed-to-soil map:"))
+                {
+                    readType = EInputReadType.seedToSoilMap;
+                }
+
+                switch(readType)
+                {
+                    case EInputReadType.none:
+                        break;
+                }
+            }
+
             Part1Solution = "TBD";
 
             Part2Solution = "TBD";
--- AdventOfCode/SolverUI.cs
+++ AdventOfCode/SolverUI.cs
@@ -271,7 +271,7 @@ namespace AdventOfCode
                     new AoC2023.Day02() { Enabled = true },
                     new AoC2023.Day03() { Enabled = true },
                     new AoC2023.Day04() { Enabled = true },
-                    new AoC2023.Day05() { Enabled = false },
+                    new AoC2023.Day05() { Enabled = true },
                     new AoC2023.Day06() { Enabled = false },
                     new AoC2023.Day07() { Enabled = false },
                     new AoC2023.Day08() { Enabled = false },
