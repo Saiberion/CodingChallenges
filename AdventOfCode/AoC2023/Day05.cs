24f45d0fee0bfdcc8e94638b2c60062ea40f23a0
Saiberion <saiberion@web.de>
2023-12-12 17:26:28 +0100

    solved part 1 of day 5

--- AdventOfCode/AoC2023/Day05.cs
+++ AdventOfCode/AoC2023/Day05.cs
@@ -1,58 +1,165 @@
 ﻿using AdventOfCode;
+using AoC2021;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AoC2023
 {
-    
+    public class SeedMapper
+    {
+        public long DestinationStart { get; set; }
+        public long SourceStart { get; set; }
+        public long RangeLength { get; set; }
+
+        public SeedMapper(string s)
+        {
+            string[] mapping = s.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            DestinationStart = long.Parse(mapping[0]);
+            SourceStart = long.Parse(mapping[1]);
+            RangeLength = long.Parse(mapping[2]);
+        }
+    }
 
     public class Day05 : Day
     {
         private enum EInputReadType
         {
             none,
-            seed,
+            seeds,
             seedToSoilMap,
             soilToFertilizterMap,
-            fertilizerToWater,
-            waterToLight,
-            lightToTemperature,
-            temperatureToHumidity,
-            humidityToLocation
+            fertilizerToWaterMap,
+            waterToLightMap,
+            lightToTemperatureMap,
+            temperatureToHumidityMap,
+            humidityToLocationMap
+        }
+
+        public static long GetID(long fromID, List<SeedMapper> map)
+        {
+            foreach(SeedMapper mapping in map)
+            {
+                if ((fromID >= mapping.SourceStart) && (fromID < (mapping.SourceStart + mapping.RangeLength)))
+                {
+                    return fromID - mapping.SourceStart + mapping.DestinationStart;
+                }
+            }
+            return fromID;
         }
 
         public override void Solve()
         {
-            List<int> seeds = new();
+            List<long> seeds = new();
+            List<SeedMapper> seedToSoil = new();
+            List<SeedMapper> soilToFertilizer = new();
+            List<SeedMapper> fertilizerToWater = new();
+            List<SeedMapper> waterToLight = new();
+            List<SeedMapper> lightToTemperature = new();
+            List<SeedMapper> temperatureToHumidity = new();
+            List<SeedMapper> humidityToLocation = new();
+            List<long> locations = new();
+
             EInputReadType readType = EInputReadType.none;
 
             foreach (string line in Input)
             {
                 if (line.StartsWith("seeds:"))
                 {
-                    string[] seedsStr = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                    foreach(string seed in seedsStr)
-                    {
-                        if (char.IsDigit(seed[0]))
-                        {
-                            seeds.Add(int.Parse(seed));
-                        }
-                    }
+                    readType = EInputReadType.seeds;
                 }
                 else if (line.StartsWith("seed-to-soil map:"))
                 {
                     readType = EInputReadType.seedToSoilMap;
+                    continue;
+                }
+                else if (line.StartsWith("soil-to-fertilizer map:"))
+                {
+                    readType = EInputReadType.soilToFertilizterMap;
+                    continue;
+                }
+                else if (line.StartsWith("fertilizer-to-water map:"))
+                {
+                    readType = EInputReadType.fertilizerToWaterMap;
+                    continue;
+                }
+                else if (line.StartsWith("water-to-light map:"))
+                {
+                    readType = EInputReadType.waterToLightMap;
+                    continue;
+                }
+                else if (line.StartsWith("light-to-temperature map:"))
+                {
+                    readType = EInputReadType.lightToTemperatureMap;
+                    continue;
+                }
+                else if (line.StartsWith("temperature-to-humidity map:"))
+                {
+                    readType = EInputReadType.temperatureToHumidityMap;
+                    continue;
+                }
+                else if (line.StartsWith("humidity-to-location map:"))
+                {
+                    readType = EInputReadType.humidityToLocationMap;
+                    continue;
+                }
+                else if (string.IsNullOrEmpty(line))
+                {
+                    continue;
                 }
 
-                switch(readType)
+                switch (readType)
                 {
                     case EInputReadType.none:
+                        break;
+                    case EInputReadType.seeds:
+                        string[] seedsStr = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        foreach (string seed in seedsStr)
+                        {
+                            if (char.IsDigit(seed[0]))
+                            {
+                                seeds.Add(long.Parse(seed));
+                            }
+                        }
+                        readType = EInputReadType.none;
+                        break;
+                    case EInputReadType.seedToSoilMap:
+                        seedToSoil.Add(new(line));
+                        break;
+                    case EInputReadType.soilToFertilizterMap:
+                        soilToFertilizer.Add(new(line));
+                        break;
+                    case EInputReadType.fertilizerToWaterMap:
+                        fertilizerToWater.Add(new(line));
+                        break;
+                    case EInputReadType.waterToLightMap:
+                        waterToLight.Add(new(line));
+                        break;
+                    case EInputReadType.lightToTemperatureMap:
+                        lightToTemperature.Add(new(line));
+                        break;
+                    case EInputReadType.temperatureToHumidityMap:
+                        temperatureToHumidity.Add(new(line));
+                        break;
+                    case EInputReadType.humidityToLocationMap:
+                        humidityToLocation.Add(new(line));
                         break;
                 }
             }
 
-            Part1Solution = "TBD";
+            foreach(long seed in seeds)
+            {
+                long nextid = GetID(seed, seedToSoil);
+                nextid = GetID(nextid, soilToFertilizer);
+                nextid = GetID(nextid, fertilizerToWater);
+                nextid = GetID(nextid, waterToLight);
+                nextid = GetID(nextid, lightToTemperature);
+                nextid = GetID(nextid, temperatureToHumidity);
+                locations.Add(GetID(nextid, humidityToLocation));
+            }
+            locations.Sort();
+
+            Part1Solution = locations[0].ToString();
 
             Part2Solution = "TBD";
         }
