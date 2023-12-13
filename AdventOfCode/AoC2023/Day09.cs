70c140e0a9ad8989911d71ec0d3762e8c8825e33
Saiberion <saiberion@web.de>
2023-12-13 18:18:16 +0100

    solved part 1 of say 9

--- AdventOfCode/AoC2023/Day09.cs
+++ AdventOfCode/AoC2023/Day09.cs
@@ -9,7 +9,51 @@ namespace AoC2023
     {
         public override void Solve()
         {
-            Part1Solution = "TBD";
+            long sumP1 = 0;
+            List<List<long>> histories = new();
+
+            foreach(string line in Input)
+            {
+                List<long> history = new();
+                string[] splitted = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string s in splitted)
+                {
+                    history.Add(long.Parse(s));
+                }
+                histories.Add(history);
+            }
+
+            foreach(List<long> hist in histories)
+            {
+                List<List<long>> differenceTree = new() { hist };
+                bool allZeroes = true;
+
+                do
+                {
+                    allZeroes = true;
+                    List<long> differences = new();
+                    for (int i = 1; i < differenceTree[^1].Count; i++)
+                    {
+                        long diff = differenceTree[^1][i] - differenceTree[^1][i - 1];
+                        if (diff != 0)
+                        {
+                            allZeroes = false;
+                        }
+                        differences.Add(diff);
+                    }
+                    differenceTree.Add(differences);
+                } while (allZeroes == false);
+
+                long newExtrapolate = 0;
+                differenceTree[^1].Add(newExtrapolate);
+                for (int t = differenceTree.Count - 2; t >= 0; t--)
+                {
+                    differenceTree[t].Add(differenceTree[t][^1] + differenceTree[t + 1][^1]);
+                }
+                sumP1 += differenceTree[0][^1];
+            }
+
+            Part1Solution = sumP1.ToString();
 
             Part2Solution = "TBD";
         }
--- AdventOfCode/SolverUI.cs
+++ AdventOfCode/SolverUI.cs
@@ -275,7 +275,7 @@ namespace AdventOfCode
                     new AoC2023.Day06() { Enabled = true },
                     new AoC2023.Day07() { Enabled = true },
                     new AoC2023.Day08() { Enabled = true },
-                    new AoC2023.Day09() { Enabled = false },
+                    new AoC2023.Day09() { Enabled = true },
                     new AoC2023.Day10() { Enabled = false },
                     new AoC2023.Day11() { Enabled = true },
                     new AoC2023.Day12() { Enabled = false },
