b6f65e38eec6cb769fcd050455fec1ebf41aee9f
Saiberion <saiberion@web.de>
2023-12-14 11:57:04 +0100

    solved part 2 of day 11

--- AdventOfCode/AoC2023/Day11.cs
+++ AdventOfCode/AoC2023/Day11.cs
@@ -13,23 +13,25 @@ namespace AoC2023
             return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
         }
 
-        public override void Solve()
+        public long GetSumOfDistances(List<List<char>> universe, int expansion)
         {
-            int sumPaths = 0;
-            List<List<char>> universe = new();
+            long sumPaths = 0;
             List<Point> galaxies = new();
 
-            foreach (string line in Input)
+            // get galaxy coordinates
+            for (int y = 0; y < universe.Count; y++)
             {
-                List<char> mapLine = new();
-                foreach (char c in line)
+                for (int x = 0; x < universe[y].Count; x++)
                 {
-                    mapLine.Add(c);
+                    if (universe[y][x] == '#')
+                    {
+                        galaxies.Add(new(x, y));
+                    }
                 }
-                universe.Add(mapLine);
             }
 
-            for (int y = 0; y < universe.Count; y++)
+            // universe expansion part 1
+            for (int y = universe.Count - 1; y >= 0; y--)
             {
                 bool noGalaxies = true;
                 for (int x = 0; x < universe[y].Count; x++)
@@ -41,12 +43,21 @@ namespace AoC2023
                 }
                 if (noGalaxies)
                 {
-                    universe.Insert(y, new List<char>(universe[y]));
-                    y++;
+                    for (int g = 0; g < galaxies.Count; g++)
+                    {
+                        if (galaxies[g].Y > y)
+                        {
+                            Point p = galaxies[g];
+                            p.Y += expansion;
+                            galaxies.RemoveAt(g);
+                            galaxies.Insert(g, p);
+                        }
+                    }
                 }
             }
 
-            for (int x = 0; x < universe[0].Count; x++)
+            // universe expansion part 2
+            for (int x = universe[0].Count - 1; x >= 0; x--)
             {
                 bool noGalaxies = true;
                 for (int y = 0; y < universe.Count; y++)
@@ -58,37 +69,49 @@ namespace AoC2023
                 }
                 if (noGalaxies)
                 {
-                    for (int y = 0; y < universe.Count; y++)
+                    for (int g = 0; g < galaxies.Count; g++)
                     {
-                        universe[y].Insert(x, '.');
-                    }
-                    x++;
-                }
-            }
-
-            for (int y = 0; y < universe.Count; y++)
-            {
-                for(int x = 0; x < universe[y].Count; x++)
-                {
-                    if (universe[y][x] == '#')
-                    {
-                        galaxies.Add(new(x, y));
+                        if (galaxies[g].X > x)
+                        {
+                            Point p = galaxies[g];
+                            p.X += expansion;
+                            galaxies.RemoveAt(g);
+                            galaxies.Insert(g, p);
+                        }
                     }
                 }
             }
 
-            for(int i = 0; i < galaxies.Count - 1; i++)
+            // get all distances
+            for (int i = 0; i < galaxies.Count - 1; i++)
             {
-                for(int j = i + 1; j < galaxies.Count; j++)
+                for (int j = i + 1; j < galaxies.Count; j++)
                 {
                     int path = GetDistance(galaxies[i], galaxies[j]);
                     sumPaths += path;
                 }
             }
+            return sumPaths;
+        }
 
-            Part1Solution = sumPaths.ToString();
+        public override void Solve()
+        {
+            List<List<char>> universe = new();
 
-            Part2Solution = "TBD";
+            // Build universe map
+            foreach (string line in Input)
+            {
+                List<char> mapLine = new();
+                foreach (char c in line)
+                {
+                    mapLine.Add(c);
+                }
+                universe.Add(mapLine);
+            }
+
+            Part1Solution = GetSumOfDistances(universe, 1).ToString();
+
+            Part2Solution = GetSumOfDistances(universe, 999999).ToString();
         }
     }
 }
