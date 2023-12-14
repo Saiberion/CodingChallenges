6b2298322b12c17dda97b50253b71c350486af26
Saiberion <saiberion@web.de>
2023-12-14 10:47:35 +0100

    solved part 1 of day 11

--- AdventOfCode/AoC2023/Day11.cs
+++ AdventOfCode/AoC2023/Day11.cs
@@ -1,15 +1,23 @@
 ﻿using AdventOfCode;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace AoC2023
 {
     public class Day11 : Day
     {
+        public static int GetDistance(Point a, Point b)
+        {
+            return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
+        }
+
         public override void Solve()
         {
+            int sumPaths = 0;
             List<List<char>> universe = new();
+            List<Point> galaxies = new();
 
             foreach (string line in Input)
             {
@@ -58,7 +66,27 @@ namespace AoC2023
                 }
             }
 
-            Part1Solution = "TBD";
+            for (int y = 0; y < universe.Count; y++)
+            {
+                for(int x = 0; x < universe[y].Count; x++)
+                {
+                    if (universe[y][x] == '#')
+                    {
+                        galaxies.Add(new(x, y));
+                    }
+                }
+            }
+
+            for(int i = 0; i < galaxies.Count - 1; i++)
+            {
+                for(int j = i + 1; j < galaxies.Count; j++)
+                {
+                    int path = GetDistance(galaxies[i], galaxies[j]);
+                    sumPaths += path;
+                }
+            }
+
+            Part1Solution = sumPaths.ToString();
 
             Part2Solution = "TBD";
         }
