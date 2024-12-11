6e86b41836f377c3e87fec0f22c23b67072dc1dd
Saiberion <saiberion@web.de>
2024-12-11 14:21:45 +0100

    prepared for part 2 but I guess I'm missing how to skip already copied file IDs

--- AdventOfCode/AoC2024/Day09.cs
+++ AdventOfCode/AoC2024/Day09.cs
@@ -1,4 +1,5 @@
 ﻿using AdventOfCode;
+using AdventOfCode.AoC2016;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,14 +8,13 @@ namespace AdventOfCode.AoC2024
 {
     public class Day09 : AoCDay
     {
-        public override void Solve()
+        private static List<int> FillDisk(string map)
         {
-            List<long> disk = [];
+            List<int> disk = [];
             bool mapValIsFile = true;
-            long nextFileID = 0;
+            int nextFileID = 0;
             
-            // fill disk
-            foreach (char c in Input[0])
+            foreach (char c in map)
             {
                 int blockSize = int.Parse(c.ToString());
                 for (int i = 0; i < blockSize; i++)
@@ -29,13 +29,16 @@ namespace AdventOfCode.AoC2024
                     }
                 }
                 mapValIsFile = !mapValIsFile;
-                if(mapValIsFile)
+                if (mapValIsFile)
                 {
                     nextFileID++;
                 }
             }
+            return disk;
+        }
 
-            // compact disk
+        private static void BlockCompact(List<int> disk)
+        {
             int nextFreeBlock = 0;
             for (; disk[nextFreeBlock] != -1; nextFreeBlock++) ;
             for (int i = disk.Count - 1; i > nextFreeBlock; i--)
@@ -44,8 +47,87 @@ namespace AdventOfCode.AoC2024
                 disk[i] = -1;
                 for (; disk[nextFreeBlock] != -1; nextFreeBlock++) ;
             }
+        }
 
-            // calculate checksum
+        private static void FileCompact(List<int> disk)
+        {
+            int nextFreeBlock = 0;
+            int nextFreeBlockSize = -1;
+            int fileToMove = disk[^1];
+            int fileSize = 0;
+
+            for (int f = disk.Count - 1; f >= 0; f--)
+            {
+                // i still have to figure out how to skip ids I already moved
+                if (disk[f] < fileToMove)
+                {
+                    if (fileSize == 0)
+                    {
+                        if (disk[f] != -1)
+                        {
+                            fileSize = 1;
+                            fileToMove = disk[f];
+                        }
+                    }
+                    else
+                    {
+                        // new file ID, try to move current ID first
+                        for (int e = 0; e < f; e++)
+                        {
+                            if (nextFreeBlockSize < 0)
+                            {
+                                if (disk[e] == -1)
+                                {
+                                    nextFreeBlock = e;
+                                    nextFreeBlockSize = 1;
+                                }
+                            }
+                            else
+                            {
+                                if (disk[e] == -1)
+                                {
+                                    nextFreeBlockSize++;
+                                }
+                                else
+                                {
+                                    // new data begin from here on, check if space is enough for fileToMove to fit
+                                    if (nextFreeBlockSize >= fileSize)
+                                    {
+                                        for (int s = 0; s < fileSize; s++)
+                                        {
+                                            disk[nextFreeBlock + s] = disk[f + 1 + s];
+                                            disk[f + 1 + s] = -1;
+                                        }
+                                        nextFreeBlockSize = -1;
+                                        break;
+                                    }
+                                    nextFreeBlockSize = -1;
+                                }
+                            }
+                        }
+                        if (disk[f] != -1)
+                        {
+                            fileToMove = disk[f];
+                            fileSize = 1;
+                        }
+                        else
+                        {
+                            fileSize = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    if ((disk[f] != -1) && (disk[f] != fileToMove))
+                    {
+                        fileSize++;
+                    }
+                }
+            }
+        }
+
+        private static long CalculateChecksum(List<int> disk)
+        {
             long checksum = 0;
             for (int i = 0; i < disk.Count; i++)
             {
@@ -54,9 +136,20 @@ namespace AdventOfCode.AoC2024
                     checksum += i * disk[i];
                 }
             }
-            Part1Solution = checksum.ToString();
+            return checksum;
+        }
 
-            Part2Solution = "TBD";
+        public override void Solve()
+        {
+            List<int> disk;
+
+            disk = FillDisk(Input[0]);
+            BlockCompact(disk);
+            Part1Solution = CalculateChecksum(disk).ToString();
+
+            disk = FillDisk(Input[0]);
+            FileCompact(disk);
+            Part2Solution = CalculateChecksum(disk).ToString();
         }
     }
 }
