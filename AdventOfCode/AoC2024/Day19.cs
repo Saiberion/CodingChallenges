d7a0e720c74bea03dac1297142d59c5981ee4626
Saiberion <saiberion@web.de>
2024-12-19 17:11:26 +0100

    again just began but didn't caem to finish a part

--- AdventOfCode/AoC2024/Day19.cs
+++ AdventOfCode/AoC2024/Day19.cs
@@ -9,6 +9,25 @@ namespace AdventOfCode.AoC2024
     {
         public override void Solve()
         {
+            List<string> towels = [];
+            List<string> patterns = [];
+            foreach (string line in Input)
+            {
+                if (line.Contains(','))
+                {
+                    foreach (string s in line.Split([','], StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        towels.Add(s);
+                    }
+                    towels.Sort(StringComparer.Create(System.Globalization.CultureInfo.);
+                }
+                else if (!string.IsNullOrEmpty(line))
+                {
+
+                }
+            }
+
+
             Part1Solution = "TBD";
 
             Part2Solution = "TBD";
