aa50dd6d351e7eeff2b6c322a2eab9a2bf748cab
Matthias Wieloch <matthias.wieloch@few-bauer.de>
2025-12-01 16:22:40 +0100

    found a way to fix my optimized solution to work for both parts

--- CodingChallenges/AdventOfCode/Year2025/Challenge01.cs
+++ CodingChallenges/AdventOfCode/Year2025/Challenge01.cs
@@ -15,6 +15,7 @@ namespace CodingChallenges.AdventOfCode.Year2025
             int password = 0;
             int passwordAlternative = 0;
             int overruns;
+            int prevDial;
 
             foreach(string line in Input)
             {
@@ -28,27 +29,35 @@ namespace CodingChallenges.AdventOfCode.Year2025
                 }
 
                 steps = int.Parse(line[1..]);
-                /*overruns = (steps / 100);
-                if (overruns > 0)
-                {
-                    passwordAlternative += overruns;
-                    steps -= overruns * 100;
-                }
+                overruns = (steps / 100);
+                passwordAlternative += overruns;
+                steps -= overruns * 100;
+                prevDial = dial;
                 dial += direction * steps;
-                if ((dial < 0) || (dial > 99))
-                {
-                    passwordAlternative++;
-                }
-                dial %= 100;
                 if (dial < 0)
                 {
+                    if (prevDial != 0)
+                    {
+                        passwordAlternative++;
+                    }
                     dial += 100;
                 }
+                if (dial > 99)
+                {
+                    if (dial > 100)
+                    {
+                        passwordAlternative++;
+                    }
+                    dial -= 100;
+                }
+                dial = Math.Abs(dial % 100);
                 if (dial == 0)
                 {
                     password++;
                     passwordAlternative++;
-                }*/
+                }
+#if BRUTE_FOCE
+                //brute force solution
                 for (int i = 0; i < steps; i++)
                 {
                     if (direction == 1)
@@ -78,6 +87,7 @@ namespace CodingChallenges.AdventOfCode.Year2025
                 {
                     password++;
                 }
+#endif
             }
 
             Part1Solution = password.ToString();
