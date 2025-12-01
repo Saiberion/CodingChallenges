a8c554ace7ac768b52537fd751cac9f9262886e8
Matthias Wieloch <matthias.wieloch@few-bauer.de>
2025-12-01 10:33:04 +0100

    Solved both parts of Day 1 But for part 2 I went down a very brute forcey way in hopes day 1 is not punishing that too hard

--- CodingChallenges/AdventOfCode/Year2025/Challenge01.cs
+++ CodingChallenges/AdventOfCode/Year2025/Challenge01.cs
@@ -9,11 +9,82 @@ namespace CodingChallenges.AdventOfCode.Year2025
     {
         public override void Solve()
         {
-            Part1Solution = "TBD";
+            int dial = 50;
+            int direction = 0;
+            int steps;
+            int password = 0;
+            int passwordAlternative = 0;
+            int overruns;
 
-            Part2Solution = "TBD";
+            foreach(string line in Input)
+            {
+                if (line[0] == 'L')
+                {
+                    direction = -1;
+                }
+                if (line[0] == 'R')
+                {
+                    direction = 1;
+                }
 
-            Part3Solution = "TBD";
+                steps = int.Parse(line[1..]);
+                /*overruns = (steps / 100);
+                if (overruns > 0)
+                {
+                    passwordAlternative += overruns;
+                    steps -= overruns * 100;
+                }
+                dial += direction * steps;
+                if ((dial < 0) || (dial > 99))
+                {
+                    passwordAlternative++;
+                }
+                dial %= 100;
+                if (dial < 0)
+                {
+                    dial += 100;
+                }
+                if (dial == 0)
+                {
+                    password++;
+                    passwordAlternative++;
+                }*/
+                for (int i = 0; i < steps; i++)
+                {
+                    if (direction == 1)
+                    {
+                        dial++;
+                    }
+                    else
+                    {
+                        dial--;
+                    }
+
+                    if (dial < 0)
+                    {
+                        dial = 99;
+                    }
+                    else if (dial > 99)
+                    {
+                        dial = 0;
+                    }
+
+                    if (dial == 0)
+                    {
+                        passwordAlternative++;
+                    }
+                }
+                if (dial == 0)
+                {
+                    password++;
+                }
+            }
+
+            Part1Solution = password.ToString();
+
+            Part2Solution = passwordAlternative.ToString();
+
+            Part3Solution = "";
         }
     }
 }
