1ed2c8cd517b8108c39c2175a2b10ccbe96651de
Matthias Wieloch <matthias.wieloch@few-bauer.de>
2025-12-02 12:59:33 +0100

    solved part 1 of day 2

--- CodingChallenges/AdventOfCode/Year2025/Challenge02.cs
+++ CodingChallenges/AdventOfCode/Year2025/Challenge02.cs
@@ -7,13 +7,41 @@ namespace CodingChallenges.AdventOfCode.Year2025
 {
     public class Challenge02 : Challenge
     {
+        private bool IdIsValid(string id)
+        {
+            if ((id.Length % 2) == 0)
+            {
+                if (id[0..(id.Length / 2)].Equals(id[(id.Length / 2)..]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
         public override void Solve()
         {
-            Part1Solution = "TBD";
+            long resultP1 = 0;
+            string[] ranges = Input[0].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(string range in ranges)
+            {
+                string[] rangeLimits = range.Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+                for(long l = long.Parse(rangeLimits[0]); l<= long.Parse(rangeLimits[1]); l++)
+                {
+                    if (!IdIsValid(l.ToString()))
+                    {
+                        resultP1 += l;
+                    }
+                }
+            }
+
+            Part1Solution = resultP1.ToString();
 
             Part2Solution = "TBD";
 
-            Part3Solution = "TBD";
+            Part3Solution = "";
         }
     }
 }
--- CodingChallenges/SolverUI.cs
+++ CodingChallenges/SolverUI.cs
@@ -357,7 +357,7 @@ namespace CodingChallenges
                 new List<Challenge>()
                 {
                     new AdventOfCode.Year2025.Challenge01() { Enabled = true },
-                    new AdventOfCode.Year2025.Challenge02() { Enabled = false },
+                    new AdventOfCode.Year2025.Challenge02() { Enabled = true },
                     new AdventOfCode.Year2025.Challenge03() { Enabled = false },
                     new AdventOfCode.Year2025.Challenge04() { Enabled = false },
                     new AdventOfCode.Year2025.Challenge05() { Enabled = false },
