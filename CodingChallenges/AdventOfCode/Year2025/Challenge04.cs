3fb34303d796d5e65c67b2c4c46027eb2a5839aa
Matthias Wieloch <matthias.wieloch@few-bauer.de>
2025-12-04 10:42:21 +0100

    solved part 2 of day 4

--- CodingChallenges/AdventOfCode/Year2025/Challenge04.cs
+++ CodingChallenges/AdventOfCode/Year2025/Challenge04.cs
@@ -7,62 +7,62 @@ namespace CodingChallenges.AdventOfCode.Year2025
 {
     public class Challenge04 : Challenge
     {
-        static private int CheckAdjecent(List<string> map, int x, int y)
+        static private int CheckAdjecent(char[,] map, int x, int y)
         {
             int blockedSpaces = 0;
 
             if (x > 0)
             {
-                if (map[y][x - 1] == '@')
+                if (map[x - 1, y] != '.')
                 {
                     blockedSpaces++;
                 }
             }
-            if (x < (map[y].Length - 1))
+            if (x < (map.GetLength(0) - 1))
             {
-                if (map[y][x + 1] == '@')
+                if (map[x + 1, y] != '.')
                 {
                     blockedSpaces++;
                 }
             }
             if (y > 0)
             {
-                if (map[y - 1][x] == '@')
+                if (map[x, y - 1] != '.')
                 {
                     blockedSpaces++;
                 }
             }
-            if (y < (map.Count - 1))
+            if (y < (map.GetLength(1) - 1))
             {
-                if (map[y + 1][x] == '@')
+                if (map[x, y + 1] != '.')
                 {
                     blockedSpaces++;
                 }
             }
             if ((x > 0) && (y > 0))
             {
-                if (map[y - 1][x - 1] == '@')
+                if (map[x - 1, y - 1] != '.')
                 {
                     blockedSpaces++;
                 }
             }
-            if ((x < (map[y].Length - 1)) && (y < (map.Count - 1)))
+            if ((x < (map.GetLength(0) - 1)) && (y < (map.GetLength(1) - 1)))
             {
-                if (map[y + 1][x + 1] == '@')
+                if (map[x + 1, y + 1] != '.')
                 {
                     blockedSpaces++;
                 }
             }
-            if ((x < (map[y].Length - 1)) && (y > 0))
+            if ((x < (map.GetLength(0) - 1)) && (y > 0))
             {
-                if (map[y - 1][x + 1] == '@')
+                if (map[x + 1, y - 1] != '.')
                 {
                     blockedSpaces++;
                 }
             }
-            if ((x > 0) && (y < (map.Count - 1)))
+            if ((x > 0) && (y < (map.GetLength(1) - 1)))
             {
-                if (map[y + 1][x - 1] == '@')
+                if (map[x - 1, y + 1] != '.')
                 {
                     blockedSpaces++;
                 }
@@ -71,30 +71,85 @@ namespace CodingChallenges.AdventOfCode.Year2025
             return blockedSpaces;
         }
 
-        public override void Solve()
+        static private int CountAllPaperRolls(char[,] map)
         {
-            //char[,] paperStorage = new char[Input[0].Length + 2, Input.Count + 2];
-            int moveableRolls = 0;
-
-            for (int y = 0; y < Input.Count; y++)
+            int blocked = 0;
+            for (int y = 0; y < map.GetLength(1); y++)
             {
-                for (int x = 0; x < Input[y].Length; x++)
+                for (int x = 0; x < map.GetLength(0); x++)
                 {
-                    //paperStorage[x + 1, y + 1] = Input[y][x];
-                    if (Input[y][x] == '@')
+                    if (map[x, y] == '@')
                     {
-                        if (CheckAdjecent(Input, x, y) < 4)
+                        blocked++;
+                    }
+                }
+            }
+            return blocked;
+        }
+
+        static private int CountAccessablePaperRolls(char[,] map, bool freeAccessableSpace)
+        {
+            int removeable = 0;
+
+            for (int y = 0; y < map.GetLength(1); y++)
+            {
+                for (int x = 0; x < map.GetLength(0); x++)
+                {
+                    if (map[x, y] != '.')
+                    {
+                        if (CheckAdjecent(map, x, y) < 4)
                         {
-                            moveableRolls++;
+                            removeable++;
+                            if (freeAccessableSpace)
+                            {
+                                map[x, y] = 'A';
+                            }
                         }
                     }
                 }
             }
 
+            if (freeAccessableSpace)
+            {
+                for (int y = 0; y < map.GetLength(1); y++)
+                {
+                    for (int x = 0; x < map.GetLength(0); x++)
+                    {
+                        if (map[x, y] == 'A')
+                        {
+                            map[x, y] = '.';
+                        }
+                    }
+                }
+            }
+
+            return removeable;
+        }
+
+        public override void Solve()
+        {
+            char[,] paperStorage = new char[Input[0].Length, Input.Count];
+
+            for (int y = 0; y < Input.Count; y++)
+            {
+                for (int x = 0; x < Input[y].Length; x++)
+                {
+                    paperStorage[x, y] = Input[y][x];
+                }
+            }
+
+            int initialPaperRolls = CountAllPaperRolls(paperStorage);
+            int moveableRolls = CountAccessablePaperRolls(paperStorage, false);
 
             Part1Solution = moveableRolls.ToString();
 
-            Part2Solution = "TBD";
+            do
+            {
+                moveableRolls = CountAccessablePaperRolls(paperStorage, true);
+            } while (moveableRolls > 0);
+
+            int remainingPaperRolls = CountAllPaperRolls(paperStorage);
+            Part2Solution = (initialPaperRolls - remainingPaperRolls).ToString();
 
             Part3Solution = "";
         }
