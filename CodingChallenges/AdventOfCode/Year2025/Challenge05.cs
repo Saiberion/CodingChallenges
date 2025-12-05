f63cd37e2043e141afe8b710b7093dba60c01860
Matthias Wieloch <matthias.wieloch@few-bauer.de>
2025-12-05 09:51:31 +0100

    Revert "line ending stuff"
    This reverts commit 484da4ddb42c4a8a5ffd1ed5faa6ebd632102d65.

--- CodingChallenges/AdventOfCode/Year2025/Challenge05.cs
+++ CodingChallenges/AdventOfCode/Year2025/Challenge05.cs
@@ -9,25 +9,11 @@ namespace CodingChallenges.AdventOfCode.Year2025
     {
         public override void Solve()
         {
-            List<long[]> ranges = [];
-            foreach (string line in Input)
-            {
-                if (line.Contains('-'))
-                {
-                    string[] r = line.Split(['-'], StringSplitOptions.RemoveEmptyEntries);
-                    ranges.Add([long.Parse(r[0]), long.Parse(r[1])]);
-                }
-                else if (line.Length > 0)
-                {
-
-                }
-            }
-
             Part1Solution = "TBD";
 
             Part2Solution = "TBD";
 
-            Part3Solution = "";
+            Part3Solution = "TBD";
         }
     }
 }
