47eca44ee4be11dd5bace7e5ad8ca4f127cee326
Matthias Wieloch <matthias.wieloch@few-bauer.de>
2025-12-05 17:16:03 +0100

    struggling with part 2

--- CodingChallenges/AdventOfCode/Year2025/Challenge05.cs
+++ CodingChallenges/AdventOfCode/Year2025/Challenge05.cs
@@ -44,7 +44,76 @@ namespace CodingChallenges.AdventOfCode.Year2025
 
             Part1Solution = countFresh.ToString();
 
-            Part2Solution = "TBD";
+            List<long[]> rangesMerged = [];
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            int d = 0;
+
+            foreach (long[] range in ranges)
+            {
+                int i;
+                for (i = 0; i < rangesMerged.Count; i++)
+                {
+                    if (range[1] < rangesMerged[i][0])
+                    {
+                        // range is completely before checked range element
+                        rangesMerged.Insert(i, range);
+                        a++;
+                        break;
+                    }
+                    else if (range[0] > rangesMerged[i][1])
+                    {
+                        // range is after current block
+                        continue;
+                    }
+                    else if ((range[0] >= rangesMerged[i][0]) && (range[1] <= rangesMerged[i][1]))
+                    {
+                        // swallowed by the range, nothing to do
+                        b++;
+                        break;
+                    }
+                    else if (range[1] == rangesMerged[i][0])
+                    {
+                        rangesMerged[i][0] = range[0];
+                        c++;
+                        break;
+                    }
+                    else if ((range[0] <= rangesMerged[i][1]) && (range[1] > rangesMerged[i][1]))
+                    {
+                        range[0] = rangesMerged[i][1] + 1;
+                    }
+                    else if ((range[0] < rangesMerged[i][0]) && (range[1] > rangesMerged[i][0]) && (range[1] <= rangesMerged[i][1]))
+                    {
+                        // we have overlap at the lower end of the existing range
+                        rangesMerged[i][0] = range[0];
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    /*else if ((range[0] < rangesMerged[i][0]) && (range[1] >= rangesMerged[i][1]))
+                    {
+                        // we have overlap at the lower end of the existing range
+                        rangesMerged[i][0] = range[0];
+                        range[0] = rangesMerged[i][1];
+                    }*/
+                }
+                if (i == rangesMerged.Count)
+                {
+                    rangesMerged.Add(range);
+                    d++;
+                }
+            }
+
+            long idCount = 0;
+            foreach (long[] range in rangesMerged)
+            {
+                idCount += range[1] - range[0] + 1;
+            }
+
+            Part2Solution = idCount.ToString();
 
             Part3Solution = "";
         }
