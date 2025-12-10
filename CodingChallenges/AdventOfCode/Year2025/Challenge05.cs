e58aa8aa66e0758ba2b5b6aa2314e11262ed9cca
Matthias Wieloch <matthias.wieloch@few-bauer.de>
2025-12-10 13:40:04 +0100

    finally solved part 2 of day 5 merging ranges sorted by their start value was easier in logic

--- CodingChallenges/AdventOfCode/Year2025/Challenge05.cs
+++ CodingChallenges/AdventOfCode/Year2025/Challenge05.cs
@@ -44,71 +44,43 @@ namespace CodingChallenges.AdventOfCode.Year2025
 
             Part1Solution = countFresh.ToString();
 
-            List<long[]> rangesMerged = [];
-            int a = 0;
-            int b = 0;
-            int c = 0;
-            int d = 0;
+            List<long[]> sortedRanges = [];
 
             foreach (long[] range in ranges)
             {
                 int i;
-                for (i = 0; i < rangesMerged.Count; i++)
+                for (i = 0; i < sortedRanges.Count; i++)
                 {
-                    if (range[1] < rangesMerged[i][0])
+                    if (range[0] < sortedRanges[i][0])
                     {
-                        // range is completely before checked range element
-                        rangesMerged.Insert(i, range);
-                        a++;
+                        sortedRanges.Insert(i, range);
                         break;
                     }
-                    else if (range[0] > rangesMerged[i][1])
+                }
+                if (i == sortedRanges.Count)
+                {
+                    sortedRanges.Add(range);
+                }
+            }
+
+            for (int i = 0; i < sortedRanges.Count - 1; i++)
+            {
+                if (sortedRanges[i + 1][0] <= sortedRanges[i][1])
+                {
+                    if (sortedRanges[i + 1][1] <= sortedRanges[i][1])
                     {
-                        // range is after current block
-                        continue;
-                    }
-                    else if ((range[0] >= rangesMerged[i][0]) && (range[1] <= rangesMerged[i][1]))
-                    {
-                        // swallowed by the range, nothing to do
-                        b++;
-                        break;
-                    }
-                    else if (range[1] == rangesMerged[i][0])
-                    {
-                        rangesMerged[i][0] = range[0];
-                        c++;
-                        break;
-                    }
-                    else if ((range[0] <= rangesMerged[i][1]) && (range[1] > rangesMerged[i][1]))
-                    {
-                        range[0] = rangesMerged[i][1] + 1;
-                    }
-                    else if ((range[0] < rangesMerged[i][0]) && (range[1] > rangesMerged[i][0]) && (range[1] <= rangesMerged[i][1]))
-                    {
-                        // we have overlap at the lower end of the existing range
-                        rangesMerged[i][0] = range[0];
-                        break;
+                        sortedRanges.RemoveAt(i-- + 1);
                     }
                     else
                     {
-                        break;
+                        sortedRanges[i][1] = sortedRanges[i + 1][1];
+                        sortedRanges.RemoveAt(i-- + 1);
                     }
-                    /*else if ((range[0] < rangesMerged[i][0]) && (range[1] >= rangesMerged[i][1]))
-                    {
-                        // we have overlap at the lower end of the existing range
-                        rangesMerged[i][0] = range[0];
-                        range[0] = rangesMerged[i][1];
-                    }*/
-                }
-                if (i == rangesMerged.Count)
-                {
-                    rangesMerged.Add(range);
-                    d++;
                 }
             }
 
             long idCount = 0;
-            foreach (long[] range in rangesMerged)
+            foreach (long[] range in sortedRanges)
             {
                 idCount += range[1] - range[0] + 1;
             }
